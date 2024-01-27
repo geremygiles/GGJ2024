5746ab1eb92d2dd1d33d1e18292246188a6c94e4
Alejandro Diaz <alexdiaz0923@gmail.com>
2024-01-27 12:00:41 -0700

    Added Floating Pop Up Text Effect
    The floating text is controlled by performance, on a scale of 0-2.
    
    0: Very bad, receives bad reactions from audience.
    
    1: Meh, receives 'OKAY' reactions from audience.
    
    2: Very good, receives good reactions from audience.

---
+++ GGJ2024/Assets/Scripts/TextPopsUI/PingPop.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PingPop : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject PlayerObject;
+
+    [SerializeField] 
+    private GameObject popUpTextPrefab;
+
+    [SerializeField] 
+    public Color[] textColors;
+
+    [SerializeField] 
+    private float textKillTime;
+
+    [SerializeField]
+    private int performance = 0;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HitNow(performance);
+        }
+    }
+
+    public void HitNow(int skill)
+    {
+        GameObject newPopUp = Instantiate(popUpTextPrefab, PlayerObject.transform.position, Quaternion.identity);
+        newPopUp.SetActive(true);
+
+        Color textColor;
+        string[] words = new string[3];
+        int playerScore = 0;
+
+        switch(skill) {
+            case 0: // Bad case:
+                textColor = textColors[0];
+                words[0] = "Boo!"; words[1] = "You suck!"; words[2] = "Dat Dawg!";
+                playerScore = 0;
+            break;
+
+            case 1: // Meh case:
+                textColor = textColors[1];
+                words[0] = "*chuckle*"; words[1] = "Oh brother!"; words[2] = "pfft";
+                playerScore = 50;
+            break;
+
+            case 2: // Good case:
+                textColor = textColors[2];
+                words[0] = "Great!"; words[1] = "Ha Ha!"; words[2] = "LMAO!";
+                playerScore = 100;
+            break;
+
+            default: // Crickets...
+                textColor = Color.black;
+            break;
+        }
+
+        string[] feedback = {"+" + playerScore.ToString(), words[Random.Range(0, words.Length)]};
+        newPopUp.GetComponent<PingPopController>().SetTextAndMove(feedback[Random.Range(0, feedback.Length)], textColor);
+
+        Destroy(newPopUp.gameObject, textKillTime);
+    }
+}
---
+++ GGJ2024/Assets/Scripts/TextPopsUI/PingPopController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PingPopController : MonoBehaviour
+{
+    private TextMeshProUGUI myText;
+
+    [SerializeField]
+    private float moveAmnt;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private Vector3[] moveDirs;
+    private Vector3 myMoveDir;
+
+    private bool canMove = false;
+
+    private void Start()
+    {
+        moveDirs = new Vector3[] {
+            transform.up, 
+            transform.up + transform.right/4,
+            transform.up + -transform.right/4
+        };
+
+        myMoveDir = moveDirs[Random.Range(0, moveDirs.Length)];
+    }
+
+    private void Update()
+    {
+        if (canMove) 
+        {
+            Debug.Log(myMoveDir);
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + myMoveDir, moveAmnt * (moveSpeed * Time.deltaTime));
+        }
+    }
+
+    public void SetTextAndMove(string textStr, Color textColor)
+    {
+        myText = GetComponentInChildren<TextMeshProUGUI>();
+
+        // I want to change this to be textColor, not just green
+        myText.color = textColor;
+        myText.text = textStr;
+        canMove = true;
+    }
+}
